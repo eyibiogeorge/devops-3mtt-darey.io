bb127a2f150ec2c937ba6b1274ea38fb725493c8
eyibiogeorge <eyibiogeorge@gmail.com>
2025-04-16 23:05:41 +0100

    file name changed

--- Linux-shell-scripting-comment/comment.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# This is another way to create
-# a multi-line comment. Easch line
-# is prefixed with a # symbol.
-echo "Here is an actual code that gets executed"
---
+++ Linux-shell-scripting-comment/commented_script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# create folder
+mkdir comment1
+mkdir comment2
+
+<<COMMENT
+Display the contents of the current directory
+and hiden folders.
+COMMENT
+ls -latr
