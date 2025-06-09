8cf5afc54cf7c2450ce5f388916861045cc6c8b0
eyibiogeorge <eyibiogeorge@gmail.com>
2025-06-09 17:29:49 +0100

    README file and images for environment variable and environment infrastructure

---
+++ Environment-Variable-and-Infrastructure-Environment/aws_cloud_manager.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Accessing the first argument
+ENVIRONMENT=$1
+
+# Acting based on the argument value
+if [ "$ENVIRONMENT" == "local" ]; then
+  echo "Running script for Local Environment..."
+elif [ "$ENVIRONMENT" == "testing" ]; then
+  echo "Running script for Testing Environment..."
+elif [ "$ENVIRONMENT" == "production" ]; then
+  echo "Running script for Production Environment..."
+else
+  echo "Invalid environment specified. Please use 'local', 'testing', or 'production'."
+  exit 2
+fi
+
